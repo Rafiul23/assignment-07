7be5cfab43ad9f342860ec0f42452f823406987f
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-09-14 22:24:42 +0600

    added fake data in json formate

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
      
-      <h1 className='text-5xl font-bold text-red-600'>Hello React</h1>
+      <h1 className='text-5xl pt-4 font-bold text-center'>Course Registration</h1>
       
       
     </>
